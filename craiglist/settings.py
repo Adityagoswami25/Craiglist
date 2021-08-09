c47242f69d9595dbb7ffe41dcad9f4668900cb0f
Aditya goswami <giriaditya26@gmail.com>
2021-08-09 12:14:29 +0530

    url updated

--- craiglist/settings.py
+++ craiglist/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'ym6#-7x)a_&i$j3_ss)oov1h70%fbst!kk3pnou*qrmx5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
